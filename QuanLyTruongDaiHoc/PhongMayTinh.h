65fffc4135e1bff7cd61cab0a9486d4064888c63
minhtruong15 <minhtruong1582005@gmail.com>
2024-06-02 15:21:46 +0700

    phonghoc

---
+++ QuanLyTruongDaiHoc/PhongMayTinh.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef PhongMayTinh_H
+#define PhongMayTinh_H
+#include "PhongHoc.h"
+class PhongMayTinh: public PhongHoc
+{
+private:
+	int soMayTinh;
+public:
+	PhongMayTinh(const string& maPhong, const string& dayNha, float dienTich, int soBongDen, int soMayTinh);
+	void setsoMayTinh(int soMayTinh);
+	int getsoMayTinh() const;
+	bool isDuAnhSang() const;
+	virtual bool datChuan() const override;
+	virtual void hienThiThongTin() const override;
+};
+#endif
+
